71304783e8a16371b4f09c349f858acfef8b1eb1
UDIT RAJ SINGH <95999823+uditthakur2001@users.noreply.github.com>
2024-08-05 12:21:56 +0530

    Create protect_branch.sh

---
+++ protect_branch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Set variables
+GITHUB_TOKEN="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"
+OWNER="uditthakur2001"
+REPO="rules-repo"
+BRANCH="news-repo"
+
+# API URL
+API_URL="https://api.github.com/repos/$OWNER/$REPO/branches/$BRANCH/protection"
+
+# Define protection rules
+read -r -d '' PROTECTION_RULES << EOF
+{
+  "required_status_checks": {
+    "strict": true,
+    "contexts": ["ci/test", "ci/lint"]
+  },
+  "enforce_admins": true,
+  "required_pull_request_reviews": {
+    "dismissal_restrictions": {},
+    "dismiss_stale_reviews": true,
+    "require_code_owner_reviews": true,
+    "required_approving_review_count": 2
+  },
+  "restrictions": null
+}
+EOF
+
+# Send request to GitHub API
+curl -X PUT \
+-H "Authorization: token $GITHUB_TOKEN" \
+-H "Accept: application/vnd.github.v3+json" \
+"$API_URL" \
+-d "$PROTECTION_RULES"
